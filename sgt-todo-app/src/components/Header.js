aecd9c20e1ad7fee4521181ce19f353bb17ff1fc
Tim Day <timmday.info@gmail.com>
2018-04-11 16:19:48 +0200

    fixed header className inheritance

--- sgt-todo-app/src/components/Header.js
+++ sgt-todo-app/src/components/Header.js
@@ -2,8 +2,7 @@ import React from 'react';
 
 const Header = (props) => (
     <div className="container">
-        <header>
-
+        <header className="header">
             <h1 className="header__title">{props.title}</h1>
             {props.subtitle && <h2 className="header__subtitle">{props.subtitle}</h2>}
         </header>
